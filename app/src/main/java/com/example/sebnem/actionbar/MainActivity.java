8677512093cb795fff22bf565b96bcada462cdab
mahammad.ismayilov <m.ismayilov559@gmail.com>
2022-10-21 14:24:47 +0400

    change

--- app/src/main/java/com/example/sebnem/actionbar/MainActivity.java
+++ app/src/main/java/com/example/sebnem/actionbar/MainActivity.java
@@ -13,17 +13,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    Fragment settingFragment  , aboutFragment, privancyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
-
-         settingFragment = new Settings();
-         aboutFragment = new AboutMe();
-         privancyFragment = new Privancy();
-        setFragment(settingFragment);
     }
 
     @Override
@@ -32,22 +26,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == id.about){
-            setFragment(aboutFragment);
-        }else if(item.getItemId() == id.privacy){
-            setFragment(privancyFragment);
-        }else {
-            setFragment(settingFragment);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-   private void setFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(id.framelayout, fragment);
-        fragmentTransaction.commit();
-    }
 }
